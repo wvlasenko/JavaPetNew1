a93fe2c290348ec1cf89562624a259f34c57eb08
volodymyr <wvlasenko@gmail.com>
2018-11-07 16:19:28 +0200

    100500

---
+++ src/com/java/begin/lesson1/HelloWord.java
@@ -0,0 +1,11 @@
+package com.java.begin.lesson1;
+
+import javax.swing.plaf.synth.SynthOptionPaneUI;
+import java.net.SocketOption;
+
+public class HelloWord {
+    public static void main(String[] args) {
+        System.out.println("Hello Word");
+        System.out.println("Hello");
+    }
+}
